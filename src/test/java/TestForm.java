efb33876f2eeea86189c78940ee9e5304d5dfb2c
Kate test <d.swetlov2017@gmail.com>
2024-03-13 18:13:35 +0300

    modified

---
+++ src/test/java/TestForm.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TestForm {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldSubmitForm() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        String planningDate = generateDate(6, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Светлова Екатерина");
+        $("[data-test-id='phone'] input").setValue("+79654456978");
+        $("[data-test-id='agreement']").click();
+        $("button").click();
+        $("notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+    }
+}
